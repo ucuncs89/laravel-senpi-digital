7adacd3cbd355d7e7c93e268c082528680141a90
deriifirgiawan <derifirgiawan025@gmail.com>
2024-06-08 21:30:24 +0700

    feat: delete banner

--- resources/views/dashboard/admin/index.blade.php
+++ resources/views/dashboard/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section("title", "Home Page")
 
 @section("content")
-    <div class="content-centered container-fluid content-inner mt-n5 text-center">
+    <div class="container content-inner text-center">
         <h4>Selamat Datang</h4>
         <h1>Kartu Pemegang Senpi Digital</h1>
     </div>
