a2765f6ff000a58f85cf64492f25a4947d7a12b5
deriifirgiawan <derifirgiawan025@gmail.com>
2024-06-08 21:33:10 +0700

    feat: centered content dashboard

--- resources/views/dashboard/admin/index.blade.php
+++ resources/views/dashboard/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section("title", "Home Page")
 
 @section("content")
-    <div class="container content-inner text-center">
+    <div class="content-centered container content-inner text-center">
         <h4>Selamat Datang</h4>
         <h1>Kartu Pemegang Senpi Digital</h1>
     </div>
