be11be9fa5ed081ca6fba308de39bb2d2a0f4c20
deriifirgiawan <derifirgiawan025@gmail.com>
2024-06-09 15:04:12 +0700

    feat: implement modal

--- resources/views/staffit/account/index.blade.php
+++ resources/views/staffit/account/index.blade.php
@@ -1,4 +1,5 @@
 @extends("layouts.master")
+@extends("partials.modal")
 
 @section("title", "Akun")
 
@@ -6,7 +7,7 @@
     <div class="content content-inner">
         <div class="section-content">
             <div class="mb-3 text-end">
-                <a href="#" class="btn btn-primary"> <i class="bi bi-plus-circle"></i> Tambah</a>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"> <i class="bi bi-plus-circle"></i> Tambah</button>
             </div>
             <div class="table-responsive">
                 <table id="datatable" class="table-striped table" data-toggle="data-table">
@@ -37,4 +38,47 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section("contentModal")
+    <form>
+        @csrf
+        <div class="modal-body">
+            <div class="mb-3">
+                <input type="text" class="form-control" id="nprp" placeholder="NPRP">
+            </div>
+
+            <div class="mb-3">
+                <input type="text" class="form-control" id="nama" placeholder="Nama">
+            </div>
+
+            <div class="mb-3">
+                <input type="text" class="form-control" id="pangkat" placeholder="Pangkat">
+            </div>
+
+            <div class="mb-3">
+                <input type="text" class="form-control" id="jabatan" placeholder="Jabatan">
+            </div>
+
+            <div class="mb-3">
+                <input type="text" class="form-control" id="kesatuan" placeholder="Kesatuan">
+            </div>
+
+            <div class="mb-3">
+                <input type="text" class="form-control" id="username" placeholder="Username">
+            </div>
+
+            <div class="mb-3">
+                <input type="email" class="form-control" id="email" placeholder="Email">
+            </div>
+
+            <div class="mb-3">
+                <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+
+        </div>
+        <div class="modal-footer">
+            <button type="submit" class="btn btn-primary">Kirim</button>
+        </div>
+    </form>
 @endsection
