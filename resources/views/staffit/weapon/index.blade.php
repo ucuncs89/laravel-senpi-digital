8f0398d6981f5ef6dbf2044f82bfdb9a2f78ba89
deriifirgiawan <derifirgiawan025@gmail.com>
2024-06-09 17:32:34 +0700

    feat: integration CRUD Weapon

--- resources/views/staffit/weapon/index.blade.php
+++ resources/views/staffit/weapon/index.blade.php
@@ -7,7 +7,7 @@
     <div class="content content-inner">
         <div class="section-content">
             <div class="mb-3 text-end">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"> <i class="bi bi-plus-circle"></i> Tambah</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalTambah"> <i class="bi bi-plus-circle"></i> Tambah</button>
             </div>
             <div class="table-responsive">
                 <table id="datatable" class="table table-striped" data-toggle="data-table">
@@ -22,48 +22,106 @@
                     </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Pistol</td>
-                            <td>HS</td>
-                            <td>HS 021115</td>
-                            <td>9 mm</td>
-                            <td>
-                                <span class="text-warning text-action">Edit</span>
-                                <span class="text-danger text-action">Hapus</span>
-                            </td>
-                        </tr>
+                        @foreach ($weapons as $weapon)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $weapon->jenis }}</td>
+                                <td>{{ $weapon->type }}</td>
+                                <td>{{ $weapon->nomor }}</td>
+                                <td>{{ $weapon->kaliber }}</td>
+                                <td>
+                                    <a href="javascript:void(0)" class="text-warning text-action" data-id="{{ $weapon->id }}" onclick="editSenjata(this)">Edit</a>
+                                    <form action="{{ route('staff-it-senjata-api.destroy', $weapon->id) }}" method="POST" style="display:inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-link text-danger text-action p-0 m-0">Hapus</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-@endsection
 
+    {{-- Modal Tambah Data --}}
+    <div class="modal fade" id="modalTambah" tabindex="-1" aria-labelledby="modalTambahLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form method="POST" action="{{ route('staff-it-senjata-api.store') }}">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="jenis" id="jenis" placeholder="Jenis" required>
+                        </div>
 
-@section("contentModal")
-    <form>
-        @csrf
-        <div class="modal-body">
-            <div class="mb-3">
-                <input type="text" class="form-control" name="jenis" id="jenis" placeholder="Jenis">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="type" id="type" placeholder="Type" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="nomor" id="nomor" placeholder="Nomor" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="kaliber" id="kaliber" placeholder="Kaliber" required>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Kirim</button>
+                    </div>
+                </form>
             </div>
-
-            <div class="mb-3">
-                <input type="text" class="form-control" name="type" id="type" placeholder="Type">
-            </div>
-
-            <div class="mb-3">
-                <input type="text" class="form-control" name="nomor" id="nomor" placeholder="Nomor">
-            </div>
-
-            <div class="mb-3">
-                <input type="text" class="form-control" name="kaliber" id="kaliber" placeholder="Kaliber">
-            </div>
-
         </div>
-        <div class="modal-footer">
-            <button type="submit" class="btn btn-primary">Kirim</button>
+    </div>
+
+    {{-- Modal Edit Data --}}
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form id="editForm" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="jenis" id="editJenis" placeholder="Jenis" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="type" id="editType" placeholder="Type" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="nomor" id="editNomor" placeholder="Nomor" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="kaliber" id="editKaliber" placeholder="Kaliber" required>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Kirim</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </form>
+    </div>
+
+    <script>
+        function editSenjata(element) {
+            var id = $(element).data('id');
+            $.get("{{ url('staff-it/senjata-api') }}/" + id + "/edit", function(data) {
+                console.log(data.jenis);
+                $('#editForm').attr('action', "{{ url('staff-it/senjata-api') }}/" + id);
+                $('#editJenis').val(data.jenis);
+                $('#editType').val(data.type);
+                $('#editNomor').val(data.nomor);
+                $('#editKaliber').val(data.kaliber);
+                $('#editModal').modal('show');
+            });
+        }
+    </script>
 @endsection
