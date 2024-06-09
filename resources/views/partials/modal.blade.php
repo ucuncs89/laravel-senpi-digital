5ffaf617ca31787414a26e04a60716bfeb1e6d28
deriifirgiawan <derifirgiawan025@gmail.com>
2024-06-09 15:04:03 +0700

    feat: add modal

---
+++ resources/views/partials/modal.blade.php
@@ -0,0 +1,7 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            @yield("contentModal")
+        </div>
+    </div>
+</div>
