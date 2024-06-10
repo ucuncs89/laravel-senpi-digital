7e269f9e018c1f3253a3e3d6d30cf21b6ea8f691
deriifirgiawan <derifirgiawan025@gmail.com>
2024-06-10 14:51:10 +0700

    feat: add Kartu Module

---
+++ app/Http/Controllers/user/KartuController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\user;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KartuController extends Controller
+{
+    public function Index()
+    {
+        return view('user.kartu.index');
+    }
+}
