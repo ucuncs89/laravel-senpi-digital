b109d8d4d8400168d4b2995c39256277c5285661
deriifirgiawan <derifirgiawan025@gmail.com>
2024-06-11 15:41:19 +0700

    feat: binding detail & list kartu

--- app/Http/Controllers/user/KartuController.php
+++ app/Http/Controllers/user/KartuController.php
@@ -3,17 +3,68 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\KartuPengajuan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class KartuController extends Controller
 {
     public function Index()
     {
-        return view('user.kartu.index');
+        $user = Auth::user();
+        $cards = KartuPengajuan::query()
+                ->select(
+                    'kartu_pengajuan.id',
+                    'kartu_pengajuan.kode_kartu',
+                    'kartu_pengajuan.tanggal_update',
+                    'kartu_pengajuan.status',
+                    'personil.id_personil',
+                    'personil.nrp',
+                    'personil.nama',
+                    'personil.pangkat',
+                    'personil.jabatan',
+                    'personil.kesatuan',
+                    'personil.foto_pribadi',
+                    'weapon.jenis',
+                    'weapon.type',
+                    'weapon.nomor',
+                    'weapon.kaliber'
+                )
+                ->leftJoin('personil', 'kartu_pengajuan.id_personil', '=', 'personil.id_personil')
+                ->leftJoin('weapon', 'kartu_pengajuan.id_senjata', '=', 'weapon.id')
+                ->where('kartu_pengajuan.status', '=', 'Success')
+                ->where('kartu_pengajuan.id_personil', '=', $user->personil_id)
+                ->get();
+        return view('user.kartu.index', compact('cards'));
     }
 
-    public function detail()
+    public function detail($id)
     {
-        return view('user.kartu.detail');
+        $user = Auth::user();
+        $card =  KartuPengajuan::query()
+                ->select(
+                    'kartu_pengajuan.id',
+                    'kartu_pengajuan.kode_kartu',
+                    'kartu_pengajuan.tanggal_update',
+                    'kartu_pengajuan.status',
+                    'personil.id_personil',
+                    'personil.nrp',
+                    'personil.nama',
+                    'personil.pangkat',
+                    'personil.jabatan',
+                    'personil.kesatuan',
+                    'personil.foto_pribadi',
+                    'weapon.jenis',
+                    'weapon.type',
+                    'weapon.nomor',
+                    'weapon.kaliber'
+                )
+                ->leftJoin('personil', 'kartu_pengajuan.id_personil', '=', 'personil.id_personil')
+                ->leftJoin('weapon', 'kartu_pengajuan.id_senjata', '=', 'weapon.id')
+                ->where('kartu_pengajuan.id_personil', '=', $user->personil_id)
+                ->where('kartu_pengajuan.id', '=', $id)
+                ->first();
+
+        return view('user.kartu.detail', compact('card'));
     }
 }
